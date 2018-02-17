d4ff49672eb3c3cebf4d354d192e1bc966e261c1
Jason Crawford <daxapps777@gmail.com>
2018-02-17 15:48:40 -0700

    Display header, footer, and admin bar on each page

---
+++ footer.php
@@ -0,0 +1,6 @@
+<p>copyright Footer</p>
+
+<?php wp_footer(); ?>
+
+  </body>
+</html>
--- page.php
+++ page.php
@@ -1,4 +1,4 @@
-<?php
+<?php get_header();
 
   while(have_posts()) {
     the_post(); ?>
@@ -6,5 +6,7 @@
     <h2><?php the_title(); ?></a></h2>
     <?php the_content(); ?>
   <?php }
+
+get_footer();
 
 ?>
