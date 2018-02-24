cd8026686349f537ebd224ffe7d7b9d809d89116
Jason Crawford <daxapps777@gmail.com>
2018-02-24 10:13:41 -0700

    Improve UI for Professors

--- single-professor.php
+++ single-professor.php
@@ -1,4 +1,6 @@
-<?php get_header();
+<?php 
+
+  get_header();
 
   while(have_posts()) {
     the_post(); ?>
@@ -14,7 +16,16 @@
 
     <div class="container container--narrow page-section">
       
-      <div class="generic-content"><?php the_content(); ?></div>
+      <div class="generic-content">
+        <div class="row group">
+          <div class="one-third">
+            <?php the_post_thumbnail(); ?>
+          </div>
+          <div class="two-thirds">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
 
     <?php
 
