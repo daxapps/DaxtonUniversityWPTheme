d8697f6aad69226fcfb991ed5b06944b2e89e5ea
Jason Crawford <daxapps777@gmail.com>
2018-03-04 16:55:40 -0700

    Add loading spinner icon

--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -13613,11 +13613,16 @@ function () {
   function Search() {
     _classCallCheck(this, Search);
 
+    this.resultsDiv = (0, _jquery.default)("#search-overlay__results");
     this.openButton = (0, _jquery.default)(".js-search-trigger");
     this.closeButton = (0, _jquery.default)(".search-overlay__close");
     this.searchOverlay = (0, _jquery.default)(".search-overlay");
+    this.searchField = (0, _jquery.default)("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.isSpinnerVisible = false;
+    this.previousValue;
+    this.typingTimer;
   }
 
   _createClass(Search, [{
@@ -13626,14 +13631,42 @@ function () {
       this.openButton.on("click", this.openOverlay.bind(this));
       this.closeButton.on("click", this.closeOverlay.bind(this));
       (0, _jquery.default)(document).on("keydown", this.keyPressDispatcher.bind(this));
+      this.searchField.on("keyup", this.typingLogic.bind(this));
+    }
+  }, {
+    key: "typingLogic",
+    value: function typingLogic() {
+      if (this.searchField.val() != this.previousValue) {
+        clearTimeout(this.typingTimer);
+
+        if (this.searchField.val()) {
+          if (!this.isSpinnerVisible) {
+            this.resultsDiv.html('<div class="spinner-loader"></div>');
+            this.isSpinnerVisible = true;
+          }
+
+          this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+        } else {
+          this.resultsDiv.html('');
+          this.isSpinnerVisible = false;
+        }
+      }
+
+      this.previousValue = this.searchField.val();
+    }
+  }, {
+    key: "getResults",
+    value: function getResults() {
+      this.resultsDiv.html("Imagine real search results here..");
+      this.isSpinnerVisible = false;
     }
   }, {
     key: "keyPressDispatcher",
     value: function keyPressDispatcher(e) {
-      // 's' button
-      if (e.keyCode == 83 && !this.isOverlayOpen) {
+      // 's' key
+      if (e.keyCode == 83 && !this.isOverlayOpen && !(0, _jquery.default)("input, textarea").is(':focus')) {
         this.openOverlay();
-      } // 'esc' button
+      } // 'esc' key
 
 
       if (e.keyCode == 27 && this.isOverlayOpen) {
@@ -13644,6 +13677,7 @@ function () {
     key: "openOverlay",
     value: function openOverlay() {
       this.searchOverlay.addClass("search-overlay--active");
+      console.log("overlay is open");
       (0, _jquery.default)("body").addClass("body-no-scroll");
       this.isOverlayOpen = true;
     }
@@ -13651,6 +13685,7 @@ function () {
     key: "closeOverlay",
     value: function closeOverlay() {
       this.searchOverlay.removeClass("search-overlay--active");
+      console.log("overlay is closed");
       (0, _jquery.default)("body").removeClass("body-no-scroll");
       this.isOverlayOpen = false;
     }
