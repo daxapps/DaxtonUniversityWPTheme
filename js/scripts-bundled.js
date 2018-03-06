acb4376b4df06f962c7157256a09fa783c662fab
Jason Crawford <daxapps777@gmail.com>
2018-03-05 20:05:51 -0700

    Make root search url universal

--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -13659,7 +13659,7 @@ function () {
     value: function getResults() {
       var _this = this;
 
-      _jquery.default.getJSON('http://localhost:3000/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
+      _jquery.default.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
         _this.resultsDiv.html("\n        <h2 class=\"search-overlay__section-title\">General Information</h2>\n        ".concat(posts.length ? '<ul class="link-list min-list">' : '<p>No general information matching your search.</p>', "\n          ").concat(posts.map(function (item) {
           return "<li><a href=\"".concat(item.link, "\">").concat(item.title.rendered, "</a></li>");
         }).join(''), "\n        ").concat(posts.length ? '</ul>' : '', "\n    "));
